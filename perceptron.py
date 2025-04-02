f4d8d349d82cab157778f995d66d308742d72387
MironDrychyts <mirondricic@gmail.com>
2025-04-02 18:12:27 +0200

    delete print of tags

--- perceptron.py
+++ perceptron.py
@@ -56,7 +56,6 @@ def main():
     iris = datasets.load_iris()
     X = iris.data[:, [2, 3]]
     y = iris.target
-    print(iris.target)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
     mc_ppn = MultiClassPerceptron(eta=0.1, n_iter=1000)
